e6912d02af41ddff66b04fe8be1f4b52fe9ac355
shimmar8 <98310391+shimmar8@users.noreply.github.com>
2025-05-23 15:42:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-terraform {
- required_providers {
-   genesyscloud = {
-     source  = "mypurecloud/genesyscloud"
-     version = "~> 1.16.0"
-   }
- }
- backend "s3" {}  
+provider "aws" {
+    region = "us-east-1"  
 }
-provider "genesyscloud" {
- oauth_client_id     = var.client_id
- oauth_client_secret = var.client_secret
- oauth_region        = var.region
-}
-resource "genesyscloud_architect_flow" "sample_flow" {
- filepath = "${path.module}/flows/sample_flow.yaml"
+
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
