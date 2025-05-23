df4dd7d6fea1c70c9f6bd6146f3402d8e883dea1
shimmar8 <98310391+shimmar8@users.noreply.github.com>
2025-05-23 17:48:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
-provider "aws" {
-    region = "us-east-1"  
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
+  terraform {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "5.19.0"  
+      }
+    }
   }
-}
+ provider "aws" {
+    region = "us-east-1" 
+    access_key = "AKIAZ7SALFO4U6JXMFZ" 
+    secret_key = "1ZvUG6UGKQskb1kU3gOjy9yeex+DXg757vC3dWm"
+  }
+ resource "aws_s3_bucket" "bucketsample34899" {
+     bucket = "bucketsample3498"
+     acl    = "private" 
+
+     tags = {
+       Name = "bucketsam123"
+       Environment = "Dev"
+     }
+ }
