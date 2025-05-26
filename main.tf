420ea1a37ef5cd4828b9512faf67f1f890ed4be9
shimmar8 <98310391+shimmar8@users.noreply.github.com>
2025-05-26 14:29:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-  terraform {
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "5.19.0"  
-      }
-    }
-  }
- provider "aws" {
-    region = "us-east-1" 
-    access_key = "AKIAZ7SALFO4U6JXMFZ" 
-    secret_key = "1ZvUG6UGKQskb1kU3gOjy9yeex+DXg757vC3dWm"
-  }
- resource "aws_s3_bucket" "bucketsample34899" {
-     bucket = "bucketsample3498"
-     acl    = "private" 
-
-     tags = {
-       Name = "bucketsam123"
-       Environment = "Dev"
-     }
- }
+provider "aws" {
+ region     = var.aws_region
+ access_key = "AKIAZ7SALFO4SYQ2J6H"
+ secret_key = "pXaAGbixOcqBXkb3sHi/rzon5lDeDDqc+w4m9Qd"
+}
+resource "aws_s3_bucket" "my_bucket" {
+ bucket = var.bucket_name
+ acl    = "private"
+}
